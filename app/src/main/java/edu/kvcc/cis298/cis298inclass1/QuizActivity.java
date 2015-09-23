39e7f56ae80150b87dc306b10a2f42329910e1fd
aaerniestudent <aaernie7528@mail.kvcc.edu>
2015-09-23 14:44:58 -0400

    adds comment "notes" into cod

--- app/src/main/java/edu/kvcc/cis298/cis298inclass1/QuizActivity.java
+++ app/src/main/java/edu/kvcc/cis298/cis298inclass1/QuizActivity.java
@@ -10,24 +10,43 @@ import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
+    //create a class level Widget variables so
+    //that we will have access to stuff from the view
+    //no value yet. just declared the variables
     private Button mTrueButton;
     private Button mFalseButton;
 
+    //I didn't write this code it was given by google
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        //fetch the widget control from the view and then
+        // cast and assign the class variable we setup
         mTrueButton = (Button) findViewById(R.id.true_button);
+
+        //now that I have a handle to the view widget, I can.
+        //setup an onclick listener for the widget.
+        //This onclicklistenr for the widget uses an anonymous inner class.
+        //we are passing what we want to have happen onClick.
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //use a toast message to print a message to the
+                //screen that will fade out after the duration
+                //listed as LENGTH_SHORT
+                //this method requires 3 parameters.
+                //the context, which will usually be Activity.this,
+                //the message, which will usually be a string from strings.xml
+                //the length, which will usually be one of the predefined constants
                 Toast.makeText(QuizActivity.this,
                                 R.string.correct_toast,
                                 Toast.LENGTH_SHORT).show();
             }
         });
 
+        //see notes from true button
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +58,8 @@ public class QuizActivity extends AppCompatActivity {
         });
     }
 
+    //these are methods that we did not write, but google provided
+    //used for menus.
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
